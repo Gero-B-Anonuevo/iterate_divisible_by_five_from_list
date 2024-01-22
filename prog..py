57452326ad22abd6631d139e1acbbe56be3ec68a
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 16:05:55 +0800

    working but has none at the end

--- prog..py
+++ prog..py
@@ -3,11 +3,9 @@
 #accept numbers
 list_of_numbers = [60, 43, 75, 44, 53, 90]
 #def function or for loop range
-def divisible_by_5(numberlist):
-    print("Given list: ", numberlist)
-    print("Divisible by 5")
-    for divisible in range(numberlist):
-        if divisible%5 == 0:
-            print(divisible)
+print("Given list:", list_of_numbers)
+print("Divisible by 5")
+for divisible in list_of_numbers:
+    if divisible%5 == 0:
+        print(divisible)
 #print results
-print (divisible_by_5(list_of_numbers))
