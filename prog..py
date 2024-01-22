c9af49311443abed8c78a06c24233c919bb41c80
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 16:08:40 +0800

    finally working

--- prog..py
+++ prog..py
@@ -1,11 +1,6 @@
-#pseudocode
-
-#accept numbers
 list_of_numbers = [60, 43, 75, 44, 53, 90]
-#def function or for loop range
 print("Given list:", list_of_numbers)
 print("Divisible by 5")
 for divisible in list_of_numbers:
     if divisible%5 == 0:
         print(divisible)
-#print results
